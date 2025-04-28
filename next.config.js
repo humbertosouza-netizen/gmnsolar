5c14049a9cc291e6ee24058bed0ca9c5db6e116e
HUMBERTO <humbirodrigues@hotmail.com>
2025-04-28 15:21:05 -0300

    fix: simplifica configurações para deploy na Vercel

--- next.config.js
+++ next.config.js
@@ -1,50 +1,9 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['gmnsolar.com.br'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
   },
   reactStrictMode: true,
-  headers: async () => {
-    return [
-      {
-        source: '/sw.js',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
-          },
-          {
-            key: 'Service-Worker-Allowed',
-            value: '/',
-          },
-        ],
-      },
-    ];
-  },
-  experimental: {
-    optimizeCss: true,
-    serverActions: true
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  transpilePackages: ['@mui/material', '@mui/icons-material']
 };
 
-module.exports = withPWA(nextConfig); 
+module.exports = nextConfig; 
